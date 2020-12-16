2c7835e99f923a9c6070dfe70310e7e26b317ffd
yennanliu <f339339@gmail.com>
2020-12-16 22:36:51 +0800

    update

--- BasicExample/project/project/ExampleBuild.scala
+++
@@ -1,6 +0,0 @@
-// import sbt._
-
-// object ExampleBuild extends Build {
-//   lazy val root = Project("basic-example", file(".")) dependsOn docker
-//   lazy val docker = file("/..").getAbsoluteFile.toURI
-// }
