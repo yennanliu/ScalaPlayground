6323467ccbd7e68a36a82d5a75ac38b15eb5ab12
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-01-27 16:50:41 +0800

    add dev/AppDev1.scala

---
+++ ScalaScraper/app/src/main/scala/dev/AppDev1.scala
@@ -0,0 +1,22 @@
+package dev
+
+import com.twitter.finagle.http.Request
+import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.http.{Controller, HttpServer}
+
+object AppDev1 extends FinatraServer
+
+class FinatraServer extends HttpServer {
+  override protected def configureHttp(router: HttpRouter): Unit = {
+    router.add[Controller01]
+    router.add[Controller02]
+  }
+}
+
+class Controller01 extends Controller {
+  get("/test1") {request: Request => "hello from FinatraServer !!!"}
+}
+
+class Controller02 extends Controller {
+  get("/test2") {request: Request => "xxxxx"}
+}
