5c1228ecd6dbb4abe49906dd7e553b16b0f22b31
yennanliu <f339339@gmail.com>
2021-02-03 07:43:51 +0800

    add ptt dev scraper, update README

---
+++ ScalaScraper/README.md
@@ -0,0 +1,11 @@
+## ScalaScraper
+- Scrapers
+	- Github Scraper
+	- PTT Scraper
+- Endpoints
+	- `http://localhost:8888/<service_type>`
+
+
+### Ref
+- https://github.com/lihaoyi/requests-scala
+- https://github.com/yennanliu/web_scraping
---
+++ ScalaScraper/app/src/main/scala/dev/AppDev3.scala
@@ -0,0 +1,26 @@
+package dev
+
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.http.{Controller, HttpServer}
+import com.twitter.util.Promise.Responder
+
+object AppDev3 extends FinatraServer3
+
+class FinatraServer3 extends HttpServer {
+  override protected def configureHttp(router: HttpRouter): Unit = {
+    router.add[Controller06]
+    router.add[Controller07]
+  }
+}
+
+class Controller06 extends Controller {
+  get("/test6") {request: Request => "hello from FinatraServer !!!"}
+}
+
+class Controller07 extends Controller {
+  val url = "https://www.ptt.cc/bbs/Soft_Job/M.1612203023.A.036.html"
+  val r = requests.get(url)
+  get("/scrap/test6") {request: Request => r.text()}
+}
+
