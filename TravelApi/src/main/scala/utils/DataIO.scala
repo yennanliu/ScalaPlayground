283c8ee05fd667c8944746a7e02b1ea6b7946fc2
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-02-20 17:01:51 +0800

    update DataIO, userUtils to load user from json data

--- TravelApi/src/main/scala/utils/DataIO.scala
+++ TravelApi/src/main/scala/utils/DataIO.scala
@@ -1,26 +1,29 @@
 package utils
 
 // https://mungingdata.com/scala/read-write-json/
+// https://github.com/lihaoyi/os-lib#getting-started
 
 class DataIO{
 
-  def getUserRecord(): Unit = {
+  def getUserRecord() = {
     os.read
     val userJson = os.read(os.pwd/"src"/"main"/"scala"/"data"/"user.json")
     val data = ujson.read(userJson)
-    println(data.value)
+    data
   }
 
-  def getOrderRecord(): Unit = {
+  def getOrderRecord() = {
     os.read
     val userJson = os.read(os.pwd/"src"/"main"/"scala"/"data"/"order.json")
     val data = ujson.read(userJson)
-    println(data.value)
+    data
   }
 }
 
-object test extends App {
-  val d = new DataIO
-  d.getUserRecord()
-  d.getOrderRecord()
-}
+//object test extends App {
+//  val d = new DataIO
+//  val users = d.getUserRecord()
+//  println(users.getClass)
+//  println("***" + users("name"))
+//  d.getOrderRecord()
+//}
--- TravelApi/src/main/scala/utils/userUtils.scala
+++ TravelApi/src/main/scala/utils/userUtils.scala
@@ -3,17 +3,28 @@ package utils
 // TO FIX : load the default user data via config
 //import data.userData
 
+import utils.DataIO
+
+import scala.collection.mutable.ArrayBuffer
+
 class userUtils {
 
-  val user_data = new data.userData
-  val userIds = user_data.userIds
-  val userPasswords = user_data.userPasswords
+  //  val user_data = new data.userData
+  //  val userIds = user_data.userIds
+  //  val userPasswords = user_data.userPasswords
+
+  val data_io = new DataIO
+  val user_data = data_io.getUserRecord()
+  val userIds = user_data("userId").arr.map(_.str)
 
   def getUser(userId: String): String ={
-    if ( userIds.contains(userId)) {
+    println("*** userIds.contains(userId) = " + userIds.contains(userId))
+    if ( userIds.contains(userId) ) {
       s"userId = $userId"
     } else {
-      s"user not existed!"
+      s"""user = $userId is not existed !
+        | existing users : $userIds
+      """.stripMargin
     }
   }
 
