40c282beb210eab24f91512dc001473a54a78aa2
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-02-20 16:13:12 +0800

    add order/user json data, update utils/DataIO.scala

--- TravelApi/src/main/scala/utils/DataIO.scala
+++ TravelApi/src/main/scala/utils/DataIO.scala
@@ -4,16 +4,23 @@ package utils
 
 class DataIO{
 
-  def getUserData(): Unit = {
+  def getUserRecord(): Unit = {
     os.read
     val userJson = os.read(os.pwd/"src"/"main"/"scala"/"data"/"user.json")
     val data = ujson.read(userJson)
     println(data.value)
+  }
 
+  def getOrderRecord(): Unit = {
+    os.read
+    val userJson = os.read(os.pwd/"src"/"main"/"scala"/"data"/"order.json")
+    val data = ujson.read(userJson)
+    println(data.value)
   }
 }
 
 object test extends App {
   val d = new DataIO
-  d.getUserData()
+  d.getUserRecord()
+  d.getOrderRecord()
 }
