333b1216e715e1495e1e5461a379630782378274
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-02-23 14:17:02 +0800

    update utils/orderUtils.scala with companion way

--- TravelApi/src/main/scala/utils/orderUtils.scala
+++ TravelApi/src/main/scala/utils/orderUtils.scala
@@ -1,13 +1,69 @@
 package utils
 
-import base.orderUtilsBase
+import scala.collection.mutable.ListBuffer
 
-class orderUtil extends orderUtilsBase {
-  override def checkOrder(orderId:String): Unit = {
-    println(s"this is order detail :  , orderId = $orderId")
-  }
+//import base.orderUtilsBase
+//
+//class orderUtil extends orderUtilsBase {
+//  override def checkOrder(orderId:String): Unit = {
+//    println(s"this is order detail :  , orderId = $orderId")
+//  }
+//
+//  override def makeOrder(userId: String): Unit = {
+//    println(s"user $userId make order!")
+//  }
+//}
 
-  override def makeOrder(userId: String): Unit = {
-    println(s"user $userId make order!")
-  }
+
+// https://github.com/yennanliu/utility_Scala/blob/master/src/main/scala/ScalaBasic/CompanionDemo5.scala
+
+// let's make the orderUtil via "companion" approach
+
+class orderUtil(UName: String, oID: String){
+  var userName:String = UName
+  var orderID:String = oID
 }
+
+object orderUtil{
+
+  var orderNum: Int = 0
+
+  //var orders:List[String] = List()
+
+  var orders: ListBuffer[String] = new ListBuffer[String]()
+
+  def makeOrder(o: orderUtil): Unit = {
+    println(o.userName + " make order !")
+    orderNum += 1
+    //orders.appended(o.orderID)
+    //orders ::: List(o.orderID)
+
+    orders += o.orderID
+  }
+
+  def showOrderNum(): Unit = {
+    println("Total order count : " + orderNum)
+  }
+
+  def showAllOrders(): Unit = {
+    println("All orders : " + orders)
+  }
+
+}
+
+// test
+//object test extends App {
+//
+//  // define orders
+//  val o1 = new orderUtil("tim", "C-0001")
+//  val o2 = new orderUtil("jack", "C-0002")
+//  val o3 = new orderUtil("vivi", "C-0003")
+//
+//  // make orders!
+//  orderUtil.makeOrder(o1)
+//  orderUtil.makeOrder(o2)
+//  orderUtil.makeOrder(o3)
+//
+//  orderUtil.orderNum
+//  orderUtil.showAllOrders
+//}
