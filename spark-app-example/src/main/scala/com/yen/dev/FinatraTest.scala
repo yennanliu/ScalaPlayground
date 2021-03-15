16921f97f9e04f149914814cba8aa170550e8893
yennanliu <f339339@gmail.com>
2021-03-15 22:33:43 +0800

    update build.sbt

---
+++ spark-app-example/src/main/scala/com/yen/dev/FinatraTest.scala
@@ -0,0 +1,10 @@
+package com.yen.dev
+
+import com.twitter.finagle.Server
+import com.twitter.finagle.http.Request
+import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.http.{Controller, HttpServer}
+
+object FinatraTest extends App {
+
+}
