60340d7073776fe183d332a36bbbdfcd7c86addc
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-10-14 18:49:13 +0800

    fix

--- urlShortener/src/main/scala/com/yen/urlShortener/controller/SimpleController.scala
+++ urlShortener/src/main/scala/com/yen/urlShortener/controller/SimpleController.scala
@@ -8,8 +8,8 @@ import com.yen.urlShortener.model.{HiRequest}
 object SimpleController {
 
   class postHelloWorld extends Controller {
-    post("/hi") { hiRequest: HiRequest =>
-      "Hello " + hiRequest.name + " with id " + hiRequest.id
+    get("/hi") { request: Request =>
+      "hello !!!"
     }
   }
 }
