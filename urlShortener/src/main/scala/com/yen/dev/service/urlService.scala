2211fe9b0584a166498e8c3ee66c3dbd5e3e06cb
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-10-15 13:33:35 +0800

    add urlService.scala, simple test

--- urlShortener/src/main/scala/com/yen/dev/service/urlService.scala
+++ urlShortener/src/main/scala/com/yen/dev/service/urlService.scala
@@ -1,6 +1,40 @@
 package com.yen.dev.service
 
-abstract class Service {
+import java.security.MessageDigest
 
-  def hashUrl(url:String)
+abstract class baseService {
+
+  // attr
+  val prefix:String
+  var urlDict:scala.collection.mutable.Map[String, String]
+
+  // method
+  def hashUrl(url:String):Option[String]
 }
+
+class urlService extends baseService {
+
+  val prefix = "https://yen.shorturl/"
+  var urlDict= scala.collection.mutable.Map(""->"")
+
+  override def hashUrl(url: String): Option[String] = {
+
+    val key = url
+
+    if (!this.urlDict.contains(key)) {
+      val value = MessageDigest.getInstance("MD5").digest(url.getBytes).toString.replace("[","")
+      //val value = url.map("0123456789abcdef".indexOf(_)).reduceLeft(_ * 16 + _).toString
+      this.urlDict += (key -> value)
+      Option(value)
+
+    }else{
+      println("url already in map, will reuse it")
+      val value = this.urlDict.get(key)
+      value
+    }
+  }
+}
+
+
+
+
---
+++ urlShortener/src/main/scala/com/yen/dev/testUrlHash.scala
@@ -0,0 +1,28 @@
+package com.yen.dev
+
+import com.yen.dev.service.urlService
+
+object testUrlHash extends App {
+
+  val url_s = new urlService()
+
+  val my_url = "www.google.com"
+  val r = url_s.hashUrl(my_url)
+  println ("r = " + r)
+  println(url_s.urlDict)
+
+  println("===============")
+
+  val my_url2 = "www.yahoo.com"
+  val r2 = url_s.hashUrl(my_url2)
+  println ("r = " + r2)
+  println(url_s.urlDict)
+
+  println("===============")
+
+  val my_url3 = "www.google.com"
+  val r3 = url_s.hashUrl(my_url3)
+  println ("r3 = " + r3)
+  println(url_s.urlDict)
+}
+
