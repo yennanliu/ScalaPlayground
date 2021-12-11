c95d916f050e3a837313002dd646b9db2c078bff
yennanliu <f339339@gmail.com>
2021-12-11 17:32:21 +0800

    update script

--- urlShortener/src/main/scala/com/yen/dev/redisTest.scala
+++ urlShortener/src/main/scala/com/yen/dev/redisTest.scala
@@ -8,19 +8,18 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
-
 object redisTest extends App {
 
   implicit val akkaSystem = akka.actor.ActorSystem()
 
   val redis = RedisClient()
 
-  print("redis = " + redis.toString)
-  print("redis = " + redis.host)
-  print("redis = " + redis.db)
-  print("redis = " + redis.port)
-  print("redis = " + redis.name)
-  print("redis = " + redis.password)
+  println(" ---> redis = " + redis.toString)
+  println("redis.host = " + redis.host)
+  println("redis.db = " + redis.db)
+  println("redis.port = " + redis.port)
+  println("redis.name = " + redis.name)
+  println("redis.password = " + redis.password)
 
   val futurePong = redis.ping()
   println("Ping sent!")
