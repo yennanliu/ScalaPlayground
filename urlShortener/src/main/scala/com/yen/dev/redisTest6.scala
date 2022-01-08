bf6b63ab70143d4637c9d6288b8ff567533fa224
yennanliu <f339339@gmail.com>
2022-01-08 19:36:20 +0800

    backup

---
+++ urlShortener/src/main/scala/com/yen/dev/redisTest6.scala
@@ -0,0 +1,12 @@
+//package com.yen.dev
+//
+///** test Redis keyExst method */
+//
+//import com.yen.urlShortener.redis.Redis
+//
+//object redisTest6 extends App{
+//  //val key1 = "my_key"
+//  val key1 = "google.com"
+//  val res1 = Redis.keyExist(key1)
+//  println("res1 = " + res1)
+//}
