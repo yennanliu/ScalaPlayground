a58bddd14546f1b7e41d3d48a16f35fcf2267ea8
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2022-03-02 16:07:46 +0800

    update

--- heroAPI/src/main/scala/com/yen/heroAPI/server/myServer2.scala
+++ heroAPI/src/main/scala/com/yen/heroAPI/server/myServer2.scala
@@ -7,16 +7,28 @@ import com.twitter.util.{Await, Future}
 
 
 object myServer2 extends TwitterServer {
+
   val service = new Service[Request, Response] {
     def apply(request: Request) = {
       val response = Response(request.version, Status.Ok)
       response.contentString = "hello"
       Future.value(response)
+
+    }
+  }
+
+  val service2 = new Service[Request, Response] {
+    def apply(request: Request) = {
+      val response = Response(request.version, Status.Ok)
+      val msg = "hello0000000"
+      println(">>>>>>>>" + msg)
+      response.contentString = msg
+      Future.value(response)
     }
   }
 
   def main(): Unit = {
-    val server = Http.serve(":8888", service)
+    val server = Http.serve(":8888", service2)
     onExit {
       server.close()
     }
