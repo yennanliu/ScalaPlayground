565dcf4f4301cfe10763afaed628ad6f0ad9a0fd
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2022-03-02 16:50:06 +0800

    update

--- heroAPI/src/main/scala/com/yen/heroAPI/server/myServer2.scala
+++ heroAPI/src/main/scala/com/yen/heroAPI/server/myServer2.scala
@@ -1,10 +1,12 @@
 package com.yen.heroAPI.server
 
 import com.twitter.finagle.{Http, Service}
-import com.twitter.finagle.http.{Request, Response, Status}
+import com.twitter.finagle.http.{HttpMuxer, Request, Response, Route, RouteIndex, Status}
 import com.twitter.server.TwitterServer
+import com.twitter.server.handler.AbortHandler
 import com.twitter.util.{Await, Future}
 
+import com.twitter.finagle.http.Method.Post
 
 object myServer2 extends TwitterServer {
 
@@ -19,6 +21,21 @@ object myServer2 extends TwitterServer {
 
   val service2 = new Service[Request, Response] {
     def apply(request: Request) = {
+      // add this to end point (so http://localhost:8888/test)
+      HttpMuxer.addHandler(
+        Route(
+          pattern = "/test/",
+          service
+        )
+      )
+
+      HttpMuxer.addHandler(
+        Route(
+          pattern = "/test2/",
+          service
+        )
+      )
+
       val response = Response(request.version, Status.Ok)
       val msg = "hello0000000"
       println(">>>>>>>>" + msg)
---
+++ heroAPI/src/main/scala/com/yen/heroAPI/service/heroService.scala
@@ -0,0 +1,5 @@
+package com.yen.heroAPI.service
+
+class heroService {
+
+}
