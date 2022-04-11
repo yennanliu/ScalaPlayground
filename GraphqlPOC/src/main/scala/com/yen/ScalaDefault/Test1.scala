dcb8d9f92294a6db1ef11e885f1f6d74ac7a6d2b
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2022-04-11 16:12:55 +0800

    update

--- GraphqlPOC/src/main/scala/com/yen/ScalaDefault/Test1.scala
+++ GraphqlPOC/src/main/scala/com/yen/ScalaDefault/Test1.scala
@@ -2,14 +2,17 @@ package com.yen.ScalaDefault
 
 object Test1 extends App {
 
-//  // example 1
-//  val url = "http://localhost:8080/graphql"
-//  val r = requests.get(url)
-//  println(r.statusCode)
+  // example 1
+  val url = "http://localhost:8080/graphql"
+  val r = requests.get(url)
+  println(r.statusCode)
+  
+  println("=======================")
 
   // example 2
   val url2 = "http://localhost:8080/graphql"
-  val data2 = "{ \"query\": \"{ allPeople { id } } \" }"
+  //val data2 = "{ \"query\": \"{ allPeople { id } } \" }"
+  val data2 = "{ \"query\": \"{ allPeople { id fullName } } \" }"
   val r2 = requests.post(
     url2,
     data = data2,
@@ -18,6 +21,7 @@ object Test1 extends App {
     )
   )
   println(r2.statusCode)
-  println(r2.contents.toString())
-  println(r2.contents.foreach(println(_)))
+//  println(r2.contents.toString())
+//  println(r2.contents.foreach(println(_)))
+  println(r2.text)
 }
