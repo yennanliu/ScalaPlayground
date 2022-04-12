01f1e34f08aa8a4d83062d22c9801d04e637c6e7
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2022-04-12 14:13:19 +0800

    fix/update

--- GraphqlPOC/src/main/scala/com/yen/Caliban3/PeopleApp.scala
+++ GraphqlPOC/src/main/scala/com/yen/Caliban3/PeopleApp.scala
@@ -1,47 +1,40 @@
-//package com.yen.Caliban3
-//
-//import sttp.client3._
-//import sttp.client3.asynchttpclient.zio.{AsyncHttpClientZioBackend, send}
-//import zio.{App, ExitCode, URIO, ZIO}
-//
-//import com.yen.Caliban3.PeopleClient._
-//
-//object PeopleApp extends App {
-//
-//  case class People(`type`: String, fullName: String, lastName: String, id: String)
-//
-//  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] = {
-//
-//    val People =
-//      (People.`type` ~
-//        People.fullName ~
-//        People.lastName ~
-//        People.id
-//        ).mapN(People)
-//
-//    val query =
-//      Query.search(Some("Berlin Ostbahnhof")) {
-//        Searchable.stations {
-//          Station.name ~
-//            Station.hasWiFi ~
-//            Station.timetable {
-//              Timetable.nextDepatures {
-//                trainInStation
-//              } ~
-//                Timetable.nextArrivals {
-//                  trainInStation
-//                }
-//            }
-//        }
-//      }
-//
-//    val uri = uri"https://api.deutschebahn.com/free1bahnql/v1/graphql"
-//
-//    send(query.toRequest(uri))
-//      .map(_.body)
-//      .absolve
-//      .tap(res => ZIO.debug(s"Result: $res"))
-//      .provideCustomLayer(AsyncHttpClientZioBackend.layer())
-//      .exitCode
-//  }
-//}
+package com.yen.Caliban3
+
+import sttp.client3._
+import sttp.client3.asynchttpclient.zio.{AsyncHttpClientZioBackend, send}
+import zio.{App, ExitCode, URIO, ZIO}
+
+import com.yen.Caliban3.PeopleClient._
+
+object PeopleApp extends App {
+
+  case class People(`type`: String, lastName: String, firstName: String, id: String)
+
+  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] = {
+
+    val allPeople =
+      ( com.yen.Caliban3.PeopleClient.allPeople.`type` ~
+        com.yen.Caliban3.PeopleClient.allPeople.`lastName` ~
+        com.yen.Caliban3.PeopleClient.allPeople.`firstName` ~
+        com.yen.Caliban3.PeopleClient.allPeople.`id`
+        ).mapN(People)
+
+    val query =
+      Query.search(Some("Berlin Ostbahnhof")) {
+        Searchable.allPeople {
+          PeopleAttr.firstName ~
+            PeopleAttr.lastName ~
+            PeopleAttr.id
+        }
+      }
+
+    val uri = uri"http://localhost:8080/graphql"
+
+    send(query.toRequest(uri))
+      .map(_.body)
+      .absolve
+      .tap(res => ZIO.debug(s"Result: $res"))
+      .provideCustomLayer(AsyncHttpClientZioBackend.layer())
+      .exitCode
+  }
+}
--- GraphqlPOC/src/main/scala/com/yen/Caliban3/PeopleClient.scala
+++ GraphqlPOC/src/main/scala/com/yen/Caliban3/PeopleClient.scala
@@ -1,51 +1,80 @@
-//package com.yen.Caliban3
-//
-//import caliban.client.CalibanClientError.DecodingError
-//import caliban.client.FieldBuilder._
-//import caliban.client._
-//import caliban.client.__Value._
-//
-//object PeopleClient {
-//
-//  sealed trait Origin extends scala.Product with scala.Serializable
-//  object Origin {
-//    case object BELT  extends Origin
-//    case object EARTH extends Origin
-//    case object MARS  extends Origin
-//
-//    implicit val decoder: ScalarDecoder[Origin] = {
-//      case __StringValue("BELT")  => Right(Origin.BELT)
-//      case __StringValue("EARTH") => Right(Origin.EARTH)
-//      case __StringValue("MARS")  => Right(Origin.MARS)
-//      case other                  => Left(DecodingError(s"Can't build Origin from input $other"))
-//    }
-//    implicit val encoder: ArgEncoder[Origin]    = {
-//      case Origin.BELT  => __EnumValue("BELT")
-//      case Origin.EARTH => __EnumValue("EARTH")
-//      case Origin.MARS  => __EnumValue("MARS")
-//    }
-//  }
-//
-//  // TODO : fix below
-//  type People
-//  object People {
-//    def firstName: SelectionBuilder[People, String]    = _root_.caliban.client.SelectionBuilder.Field("firstName", Scalar())
-//    def lastName: SelectionBuilder[People, String]    = _root_.caliban.client.SelectionBuilder.Field("lastName", Scalar())
-//    def id: SelectionBuilder[People, String]    = _root_.caliban.client.SelectionBuilder.Field("id", Scalar())
-//  }
-//
-//  type allPeople
-//  object allPeople {
-//    def `type`: SelectionBuilder[allPeople, String] =
-//      _root_.caliban.client.SelectionBuilder.Field("type", Scalar())
-//    def firstName: SelectionBuilder[allPeople, String] =
-//      _root_.caliban.client.SelectionBuilder.Field("firstName", Scalar())
-//    def lastName: SelectionBuilder[allPeople, String] =
-//      _root_.caliban.client.SelectionBuilder.Field("lastName", Scalar())
-//    def id: SelectionBuilder[allPeople, String] =
-//      _root_.caliban.client.SelectionBuilder.Field("id", Scalar())
-//    def friends: SelectionBuilder[allPeople, List[People]] =
-//      _root_.caliban.client.SelectionBuilder.Field("friends", ListOf(Scalar()))
-//  }
-//
-//}
+package com.yen.Caliban3
+
+import caliban.client.CalibanClientError.DecodingError
+import caliban.client.FieldBuilder._
+import caliban.client._
+import caliban.client.__Value._
+
+object PeopleClient {
+
+  sealed trait Origin extends scala.Product with scala.Serializable
+  object Origin {
+    case object BELT  extends Origin
+    case object EARTH extends Origin
+    case object MARS  extends Origin
+
+    implicit val decoder: ScalarDecoder[Origin] = {
+      case __StringValue("BELT")  => Right(Origin.BELT)
+      case __StringValue("EARTH") => Right(Origin.EARTH)
+      case __StringValue("MARS")  => Right(Origin.MARS)
+      case other                  => Left(DecodingError(s"Can't build Origin from input $other"))
+    }
+    implicit val encoder: ArgEncoder[Origin]    = {
+      case Origin.BELT  => __EnumValue("BELT")
+      case Origin.EARTH => __EnumValue("EARTH")
+      case Origin.MARS  => __EnumValue("MARS")
+    }
+  }
+
+  // TODO : fix below
+  type PeopleAttr
+  object PeopleAttr {
+    def firstName: SelectionBuilder[PeopleAttr, String]    = _root_.caliban.client.SelectionBuilder.Field("firstName", Scalar())
+    def lastName: SelectionBuilder[PeopleAttr, String]    = _root_.caliban.client.SelectionBuilder.Field("lastName", Scalar())
+    def id: SelectionBuilder[PeopleAttr, String]    = _root_.caliban.client.SelectionBuilder.Field("id", Scalar())
+  }
+
+  type allPeople
+  object allPeople {
+    def `type`: SelectionBuilder[allPeople, String] =
+      _root_.caliban.client.SelectionBuilder.Field("type", Scalar())
+    def firstName: SelectionBuilder[allPeople, String] =
+      _root_.caliban.client.SelectionBuilder.Field("firstName", Scalar())
+    def lastName: SelectionBuilder[allPeople, String] =
+      _root_.caliban.client.SelectionBuilder.Field("lastName", Scalar())
+    def id: SelectionBuilder[allPeople, String] =
+      _root_.caliban.client.SelectionBuilder.Field("id", Scalar())
+    def friends: SelectionBuilder[allPeople, List[String]] =
+      _root_.caliban.client.SelectionBuilder.Field("friends", ListOf(Scalar()))
+  }
+
+  type PeopleLocation
+  object OperationLocation {
+    def id: SelectionBuilder[PeopleLocation, Option[String]] =
+      _root_.caliban.client.SelectionBuilder.Field("id", OptionOf(Scalar()))
+  }
+
+  type Searchable
+  object Searchable {
+    def allPeople[A](innerSelection: SelectionBuilder[PeopleAttr, A]): SelectionBuilder[Searchable, List[A]] =
+      _root_.caliban.client.SelectionBuilder.Field("stations", ListOf(Obj(innerSelection)))
+    def operationLocations[A](
+                               innerSelection: SelectionBuilder[PeopleLocation, A]
+                             ): SelectionBuilder[Searchable, List[A]] =
+      _root_.caliban.client.SelectionBuilder.Field("operationLocations", ListOf(Obj(innerSelection)))
+    def peopleLocations[A](
+                               innerSelection: SelectionBuilder[PeopleLocation, A]
+                             ): SelectionBuilder[Searchable, List[A]] =
+      _root_.caliban.client.SelectionBuilder.Field("operationLocations", ListOf(Obj(innerSelection)))
+  }
+
+  // Query
+  type Query = _root_.caliban.client.Operations.RootQuery
+  object Query {
+    def search[A](searchTerm: Option[String] = None)(
+      innerSelection: SelectionBuilder[Searchable, A]
+    )(implicit encoder0: ArgEncoder[Option[String]]): SelectionBuilder[_root_.caliban.client.Operations.RootQuery, A] =
+      _root_.caliban.client.SelectionBuilder
+        .Field("search", Obj(innerSelection), arguments = List(Argument("searchTerm", searchTerm, "String")(encoder0)))
+  }
+}
